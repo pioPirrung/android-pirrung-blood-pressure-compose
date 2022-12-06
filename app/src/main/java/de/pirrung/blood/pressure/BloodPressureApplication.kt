b625afc22f9df8c948e5b7d583f0438afa050cb3
Chris Pirrung <pirrung@pioneo.de>
2022-12-06 16:21:57 +0100

    initial commit

---
+++ app/src/main/java/de/pirrung/blood/pressure/BloodPressureApplication.kt
@@ -0,0 +1,11 @@
+package de.pirrung.blood.pressure
+
+import android.app.Application
+
+class BloodPressureApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
