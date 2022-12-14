8dc6a5f63d8aa8b185613edf75473e5d4d7b3552
Chris Pirrung <pirrung@pioneo.de>
2022-12-14 12:21:18 +0100

    Update BloodPressureDetailViewModel.kt

--- blood-pressure/src/main/java/de/pirrung/feature/blood/pressure/presentation/blood_pressure_detail/BloodPressureDetailViewModel.kt
+++ blood-pressure/src/main/java/de/pirrung/feature/blood/pressure/presentation/blood_pressure_detail/BloodPressureDetailViewModel.kt
@@ -2,13 +2,10 @@ package de.pirrung.feature.blood.pressure.presentation.blood_pressure_detail
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import de.pirrung.feature.blood.pressure.domain.model.BloodPressure
 import de.pirrung.feature.blood.pressure.domain.use_case.DeleteBloodPressureMeasurement
 import de.pirrung.feature.blood.pressure.domain.use_case.GetBloodPressureMeasurement
-import de.pirrung.feature.blood.pressure.presentation.blood_pressure.BloodPressureEvent
 import kotlinx.coroutines.launch
 
 class BloodPressureDetailViewModel(
